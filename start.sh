44853476feb187414839827dedd81864e9682ae9
Marcos Paliari <marcos@paliari.com.br>
2018-09-06 10:51:24 -0300

    allow custom DOCUMENT_ROOT

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+if [ ! -z $DOCUMENT_ROOT ]; then
+    sed -i "s#DocumentRoot /var/www/html/public\$#DocumentRoot $DOCUMENT_ROOT#g" /etc/apache2/sites-enabled/000-default.conf
+    sed -i "s#<Directory /var/www/html/public>\$#<Directory $DOCUMENT_ROOT>#g" /etc/apache2/sites-enabled/000-default.conf
+
+    sed -i "s#DocumentRoot /var/www/html/public\$#DocumentRoot $DOCUMENT_ROOT#g" /etc/apache2/sites-enabled/default-ssl.conf
+    sed -i "s#<Directory /var/www/html/public>\$#<Directory $DOCUMENT_ROOT>#g" /etc/apache2/sites-enabled/default-ssl.conf
+
+    echo 'Document root applied'
+fi;
+
 source /etc/apache2/envvars && exec /usr/sbin/apache2 -DFOREGROUND
